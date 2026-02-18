8c581187f6aba85f0a8ec841e60773e816fda30d
Vinicius Pereira <viniciuspereiraassuncao@gmail.com>
2026-02-18 13:19:35 -0300

    [FEAT] Serve static files from the uploads directory

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,13 @@ import express from 'express'
 import cors from 'cors'
 import catchAppError from './middlewares/catch-app-error'
 import routes from './module/routes'
+import path from 'path'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes)
 app.use(catchAppError)
 
